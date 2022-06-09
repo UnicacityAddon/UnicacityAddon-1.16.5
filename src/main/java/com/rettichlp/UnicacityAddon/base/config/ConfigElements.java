7b3ea83bf276e17bfcc553b86b091064a41c5de0
rettichlp <rettichlp01@gmail.com>
2022-06-09 14:07:42 +0200

    Fix parameters in ConfigElements and ConfigSettings

--- src/main/java/com/rettichlp/UnicacityAddon/base/config/ConfigElements.java
+++ src/main/java/com/rettichlp/UnicacityAddon/base/config/ConfigElements.java
@@ -4,10 +4,6 @@ import com.rettichlp.UnicacityAddon.UnicacityAddon;
 import com.rettichlp.UnicacityAddon.base.faction.Faction;
 
 public class ConfigElements {
-    private static Faction nametagAlliance1;
-    private static Faction nametagAlliance2;
-    private static Faction nametagStreetwar1;
-    private static Faction nametagStreetwar2;
 
     public static boolean getNametagFaction() {
         return UnicacityAddon.CONFIG.has("NAMETAG_FACTION") && UnicacityAddon.CONFIG.get("NAMETAG_FACTION").getAsBoolean(); // default = false
@@ -21,43 +17,45 @@ public class ConfigElements {
         return UnicacityAddon.CONFIG.has("NAMETAG_HOUSEBAN") && UnicacityAddon.CONFIG.get("NAMETAG_HOUSEBAN").getAsBoolean(); // default = false
     }
 
+    // ALLIANCE
     public static boolean getNametagAlliance() {
         return UnicacityAddon.CONFIG.has("NAMETAG_ALLIANCE") && UnicacityAddon.CONFIG.get("NAMETAG_ALLIANCE").getAsBoolean(); // default = false
     }
 
     public static Faction getNametagAlliance1() {
-        return nametagAlliance1;
+        return UnicacityAddon.CONFIG.has("NAMETAG_ALLIANCE1") ? Faction.valueOf(UnicacityAddon.CONFIG.get("NAMETAG_ALLIANCE1").getAsString()) : Faction.NULL; // default = NULL
     }
 
     public static Faction getNametagAlliance2() {
-        return nametagAlliance2;
+        return UnicacityAddon.CONFIG.has("NAMETAG_ALLIANCE2") ? Faction.valueOf(UnicacityAddon.CONFIG.get("NAMETAG_ALLIANCE2").getAsString()) : Faction.NULL; // default = NULL
     }
 
     public static void setNametagAlliance1(Faction allianceFaction1) {
-        nametagAlliance1 = allianceFaction1;
+        UnicacityAddon.CONFIG.addProperty("NAMETAG_ALLIANCE1", allianceFaction1.toString());
     }
 
     public static void setNametagAlliance2(Faction allianceFaction2) {
-        nametagAlliance2 = allianceFaction2;
+        UnicacityAddon.CONFIG.addProperty("NAMETAG_ALLIANCE2", allianceFaction2.toString());
     }
 
+    // STREETWAR
     public static boolean getNametagStreetwar() {
         return UnicacityAddon.CONFIG.has("NAMETAG_STREETWAR") && UnicacityAddon.CONFIG.get("NAMETAG_STREETWAR").getAsBoolean(); // default = false
     }
 
     public static Faction getNametagStreetwar1() {
-        return nametagStreetwar1;
+        return UnicacityAddon.CONFIG.has("NAMETAG_STREETWAR1") ? Faction.valueOf(UnicacityAddon.CONFIG.get("NAMETAG_STREETWAR1").getAsString()) : Faction.NULL; // default = NULL
     }
 
     public static Faction getNametagStreetwar2() {
-        return nametagStreetwar2;
+        return UnicacityAddon.CONFIG.has("NAMETAG_STREETWAR2") ? Faction.valueOf(UnicacityAddon.CONFIG.get("NAMETAG_STREETWAR2").getAsString()) : Faction.NULL; // default = NULL
     }
 
     public static void setNametagStreetwar1(Faction streetwarFaction1) {
-        nametagStreetwar1 = streetwarFaction1;
+        UnicacityAddon.CONFIG.addProperty("NAMETAG_STREETWAR1", streetwarFaction1.toString());
     }
 
     public static void setNametagStreetwar2(Faction streetwarFaction2) {
-        nametagStreetwar2 = streetwarFaction2;
+        UnicacityAddon.CONFIG.addProperty("NAMETAG_STREETWAR2", streetwarFaction2.toString());
     }
 }
--- src/main/java/com/rettichlp/UnicacityAddon/base/config/ConfigSettings.java
+++ src/main/java/com/rettichlp/UnicacityAddon/base/config/ConfigSettings.java
@@ -22,10 +22,20 @@ public class ConfigSettings {
 
         // Header
         list.add(new HeaderElement(
-                Message.getBuilder().of("U").color(ColorCode.RED).bold().advance().of("nica").color(ColorCode.BLUE).bold().advance().of("C").color(ColorCode.RED).bold()
-                        .advance().of("ity").color(ColorCode.BLUE).bold().advance().of("A").color(ColorCode.RED).bold().advance().of("ddon").color(ColorCode.BLUE).bold()
-                        .advance().space().of("v" + UnicacityAddon.VERSION).color(ColorCode.BLUE).bold().advance().space().of("-").color(ColorCode.GRAY).bold().advance().space()
-                        .of("by RettichLP").color(ColorCode.GOLD).advance().create()));
+                Message.getBuilder()
+                        .of("U").color(ColorCode.RED).bold().advance()
+                        .of("nica").color(ColorCode.BLUE).bold().advance()
+                        .of("C").color(ColorCode.RED).bold().advance()
+                        .of("ity").color(ColorCode.BLUE).bold().advance()
+                        .of("A").color(ColorCode.RED).bold().advance()
+                        .of("ddon").color(ColorCode.BLUE).bold().advance()
+                        .space()
+                        .of("v" + UnicacityAddon.VERSION).color(ColorCode.BLUE).bold().advance()
+                        .space()
+                        .of("-").color(ColorCode.GRAY).bold().advance()
+                        .space()
+                        .of("by RettichLP").color(ColorCode.GOLD).advance()
+                        .create()));
 
         BooleanElement nametagFactionSuffix = new BooleanElement("Fraktionssuffix", unicacityAddon, new ControlElement.IconData(Material.NAME_TAG), "NAMETAG_FACTIONSUFFIX",
                 ConfigElements.getNametagFactionSuffix());
@@ -35,12 +45,12 @@ public class ConfigSettings {
                 ConfigElements.getNametagFaction());
         list.add(nameTagFaction);
 
-        BooleanElement nameTagAlliance = new BooleanElement("Bündnismarkierung", unicacityAddon, new ControlElement.IconData(Material.PINK_DYE), "NAMETAG_ALLIANCE",
+        BooleanElement nameTagAlliance = new BooleanElement("Bündnismarkierung", unicacityAddon, new ControlElement.IconData(Material.PURPLE_DYE), "NAMETAG_ALLIANCE",
                 ConfigElements.getNametagAlliance());
         nameTagAlliance.setSubSettings(nameTagAllianceSettings());
         list.add(nameTagAlliance);
 
-        BooleanElement nameTagStreetwar = new BooleanElement("Streetwarmarkierung", unicacityAddon, new ControlElement.IconData(Material.IRON_SWORD), "NAMETAG_STREETWAR",
+        BooleanElement nameTagStreetwar = new BooleanElement("Streetwarmarkierung", unicacityAddon, new ControlElement.IconData(Material.DIAMOND_HORSE_ARMOR), "NAMETAG_STREETWAR",
                 ConfigElements.getNametagStreetwar());
         nameTagStreetwar.setSubSettings(nameTagStreetwarSettings());
         list.add(nameTagStreetwar);
@@ -57,14 +67,14 @@ public class ConfigSettings {
         settings.add(new HeaderElement(Message.getBuilder().of("müssen beide Felder auf der selben stehen.").advance().create()));
 
         DropDownMenu<Faction> dropDownMenu1 = new DropDownMenu<Faction>("Bündnisfraktion 1", 0, 0, 0, 0).fill(Faction.values());
-        dropDownMenu1.setSelected(Faction.NULL);
+        dropDownMenu1.setSelected(ConfigElements.getNametagAlliance1());
         dropDownMenu1.setEntryDrawer((object, matrixStack, x, y, string) -> LabyMod.getInstance().getDrawUtils()
                 .drawString(matrixStack, Faction.valueOf(object.toString().toUpperCase()).getDisplayName(), x, y));
         DropDownElement<Faction> dropDownElement1 = new DropDownElement<>("", dropDownMenu1);
         dropDownElement1.setChangeListener(ConfigElements::setNametagAlliance1);
 
         DropDownMenu<Faction> dropDownMenu2 = new DropDownMenu<Faction>("Bündnisfraktion 2 (optional)", 0, 0, 0, 0).fill(Faction.values());
-        dropDownMenu2.setSelected(Faction.NULL);
+        dropDownMenu2.setSelected(ConfigElements.getNametagAlliance2());
         dropDownMenu2.setEntryDrawer((object, matrixStack, x, y, string) -> LabyMod.getInstance().getDrawUtils()
                 .drawString(matrixStack, Faction.valueOf(object.toString().toUpperCase()).getDisplayName(), x, y));
         DropDownElement<Faction> dropDownElement2 = new DropDownElement<>("", dropDownMenu2);
@@ -82,14 +92,14 @@ public class ConfigSettings {
         settings.add(new HeaderElement(Message.getBuilder().of("müssen beide Felder auf der selben stehen.").advance().create()));
 
         DropDownMenu<Faction> dropDownMenu1 = new DropDownMenu<Faction>("Streetwarfraktion 1", 0, 0, 0, 0).fill(Faction.values());
-        dropDownMenu1.setSelected(Faction.NULL);
+        dropDownMenu1.setSelected(ConfigElements.getNametagStreetwar1());
         dropDownMenu1.setEntryDrawer((object, matrixStack, x, y, string) -> LabyMod.getInstance().getDrawUtils()
                 .drawString(matrixStack, Faction.valueOf(object.toString().toUpperCase()).getDisplayName(), x, y));
         DropDownElement<Faction> dropDownElement1 = new DropDownElement<>("", dropDownMenu1);
         dropDownElement1.setChangeListener(ConfigElements::setNametagStreetwar1);
 
         DropDownMenu<Faction> dropDownMenu2 = new DropDownMenu<Faction>("Streetwarfraktion 2 (optional)", 0, 0, 0, 0).fill(Faction.values());
-        dropDownMenu2.setSelected(Faction.NULL);
+        dropDownMenu2.setSelected(ConfigElements.getNametagStreetwar2());
         dropDownMenu2.setEntryDrawer((object, matrixStack, x, y, string) -> LabyMod.getInstance().getDrawUtils()
                 .drawString(matrixStack, Faction.valueOf(object.toString().toUpperCase()).getDisplayName(), x, y));
         DropDownElement<Faction> dropDownElement2 = new DropDownElement<>("", dropDownMenu2);
