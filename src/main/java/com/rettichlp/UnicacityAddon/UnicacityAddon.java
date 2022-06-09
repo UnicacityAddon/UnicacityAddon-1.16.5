1cc212b9fdad1a217cb0575fbab4ddc2c4c63a3c
rettichlp <rettichlp01@gmail.com>
2022-06-09 12:55:16 +0200

    Move config generation from UnicacityAddon to ConfigSettings

--- src/main/java/com/rettichlp/UnicacityAddon/UnicacityAddon.java
+++ src/main/java/com/rettichlp/UnicacityAddon/UnicacityAddon.java
@@ -3,18 +3,12 @@ package com.rettichlp.UnicacityAddon;
 import com.google.gson.JsonObject;
 import com.rettichlp.UnicacityAddon.base.abstraction.AbstractionLayer;
 import com.rettichlp.UnicacityAddon.base.command.CommandHandler;
-import com.rettichlp.UnicacityAddon.base.config.ConfigElements;
+import com.rettichlp.UnicacityAddon.base.config.ConfigSettings;
 import com.rettichlp.UnicacityAddon.base.faction.FactionHandler;
-import com.rettichlp.UnicacityAddon.base.text.ColorCode;
-import com.rettichlp.UnicacityAddon.base.text.Message;
 import com.rettichlp.UnicacityAddon.events.NameTagEventHandler;
 import net.labymod.api.LabyModAddon;
 import net.labymod.main.LabyMod;
-import net.labymod.settings.elements.BooleanElement;
-import net.labymod.settings.elements.ControlElement;
-import net.labymod.settings.elements.HeaderElement;
 import net.labymod.settings.elements.SettingsElement;
-import net.labymod.utils.Material;
 import net.minecraft.client.Minecraft;
 
 import java.util.List;
@@ -49,42 +43,6 @@ public class UnicacityAddon extends LabyModAddon {
 
     @Override
     protected void fillSettings(List<SettingsElement> list) {
-
-        list.add(new HeaderElement(Message.getBuilder()
-                .of("U").color(ColorCode.RED).bold().advance()
-                .of("nica").color(ColorCode.BLUE).bold().advance()
-                .of("C").color(ColorCode.RED).bold().advance()
-                .of("ity").color(ColorCode.BLUE).bold().advance()
-                .of("A").color(ColorCode.RED).bold().advance()
-                .of("ddon").color(ColorCode.BLUE).bold().advance()
-                .space()
-                .of("v" + VERSION).color(ColorCode.BLUE).bold().advance()
-                .space()
-                .of("-").color(ColorCode.GRAY).bold().advance()
-                .space()
-                .of("by RettichLP").color(ColorCode.GOLD).advance()
-                .create()));
-
-        BooleanElement nameTagForFactionSuffix = new BooleanElement("Fraktionssuffix", this, new ControlElement.IconData(Material.NAME_TAG), "NAMETAG_FACTIONSUFFIX",
-                ConfigElements.NAMETAG_FACTIONSUFFIX);
-        list.add(nameTagForFactionSuffix);
-
-        BooleanElement nameTagForFaction = new BooleanElement("Fraktionsmarkierung", this, new ControlElement.IconData(Material.BLUE_DYE), "NAMETAG_FACTION",
-                ConfigElements.NAMETAG_FACTION);
-        list.add(nameTagForFaction);
-
-        BooleanElement nameTagForAlliance = new BooleanElement("Bündnismarkierung", this, new ControlElement.IconData(Material.PINK_DYE), "NAMETAG_ALLIANCE",
-                ConfigElements.NAMETAG_ALLIANCE);
-        nameTagForAlliance.setSubSettings(ConfigElements.getNameTagForAlliance());
-        list.add(nameTagForAlliance);
-
-        BooleanElement nameTagForStreetwar = new BooleanElement("Streetwarmarkierung", this, new ControlElement.IconData(Material.IRON_SWORD), "NAMETAG_STREETWAR",
-                ConfigElements.NAMETAG_STREETWAR);
-        nameTagForStreetwar.setSubSettings(ConfigElements.getNameTagForStreetwar());
-        list.add(nameTagForStreetwar);
-
-        BooleanElement nameTagForHouseBan = new BooleanElement("Hausverbot", this, new ControlElement.IconData(Material.SPAWNER), "NAMETAG_HOUSEBAN",
-                ConfigElements.NAMETAG_HOUSEBAN);
-        list.add(nameTagForHouseBan);
+        ConfigSettings.createConfig(this, list);
     }
 }
