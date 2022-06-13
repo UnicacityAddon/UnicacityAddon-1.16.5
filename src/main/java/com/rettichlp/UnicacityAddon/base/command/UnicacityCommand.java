5bf25d57cae8d87edac1239b3c3c7da04f5883a9
rettichlp <rettichlp01@gmail.com>
2022-06-13 10:47:48 +0200

    Re-Enable CommandHandler and fix crash on game start

--- src/main/java/com/rettichlp/UnicacityAddon/base/command/UnicacityCommand.java
+++ src/main/java/com/rettichlp/UnicacityAddon/base/command/UnicacityCommand.java
@@ -2,10 +2,12 @@ package com.rettichlp.UnicacityAddon.base.command;
 
 import com.rettichlp.UnicacityAddon.base.abstraction.UPlayer;
 
+import java.util.List;
+
 /**
  * @author RettichLP
  */
 public interface UnicacityCommand {
 
-    boolean onCommand(UPlayer p, String[] args);
+    boolean onCommand(UPlayer p, List<String> args);
 }
